dd4a87bee913d703df6a60ea9b4d0c44fb1d1792
MohamadSheikhAhmad <sheikh.ahmad.moh@gmail.com>
2023-09-17 00:26:16 +0300

    login

--- Routes/Controllers/LogInController.js
+++ Routes/Controllers/LogInController.js
@@ -5,10 +5,12 @@ router.post("/", async (req, res) => {
   const result = await req.service.login(req);
   if (typeof result === String) {
     if (result.startsWith("Incorrect") || result.startsWith("failed")) {
-      res.send(400, result);
+      res.send(400, { success: false, result });
     }
   } else {
-    res.status(200).json({ accessToken: result, role: req.user.role });
+    res
+      .status(200)
+      .json({ success: true, accessToken: result, role: req.user.role });
   }
 });
 
